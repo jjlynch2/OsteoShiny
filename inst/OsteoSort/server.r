a00de83f66d0197d74661fa02205b3f5d1b1c618
Jeff Lynch <jjlynch@osteocoder.com>
2017-12-17 17:25:08 +1000

    Add files via upload

--- inst/OsteoSort/server.r
+++ inst/OsteoSort/server.r
@@ -93,7 +93,7 @@ shinyServer(function(input, output, session) {
 
 
 			target <- paste('"', file.path(R.home("bin"), "R.exe"), " -e ", "library(OsteoShiny);OsteoSort()", '"', sep="")
-			arguments <- paste'"', "-e ", "library(OsteoShiny);OsteoSort()", '"', sep="")
+			arguments <- paste('"', "-e ", "library(OsteoShiny);OsteoSort()", '"', sep="")
 			icon <- paste('"', system.file("vbs/OsteoSort.ico", package = "OsteoShiny"), '"', sep="")
 			pathname <- paste('"', paste(gsub("/Documents", "", file.path(path.expand("~"), "Desktop") ), "OsteoSort.lnk", sep = "/"), '"', sep="")
 			vbs <- paste('"', system.file("vbs/createLink.vbs", package = "OsteoShiny"), '"', sep="")
