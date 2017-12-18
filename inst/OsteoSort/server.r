ebec99396798c58f829e7b0c7dce35f809898067
Jeff Lynch <jjlynch@osteocoder.com>
2017-12-18 10:09:17 +1000

    Add files via upload

--- inst/OsteoSort/server.r
+++ inst/OsteoSort/server.r
@@ -6,13 +6,10 @@
 
 library(shiny)
 library(plyr)	#loaded for multiple.r
-
 options(shiny.maxRequestSize=30*1024^2) #increased file upload size to 30MB
 options(warn = -1) #disables warnings
 
 shinyServer(function(input, output, session) {
-
-
 	################generates temporary directories for multiuser environment
 	workingdd <- getwd()
 	sessiontempd <- OsteoSort:::randomstring(n = 1, length = 12)
@@ -22,15 +19,14 @@ shinyServer(function(input, output, session) {
 	setwd(sessiontempd)
 	sessiontemp <- getwd()
 
-
 	#defines which modules to include
 	source("../../server/multiple.r", local=TRUE) ###imports multiple comparison server code
 	source("../../server/single.r", local=TRUE) ###imports single comparison server code
 	source("../../server/metric.r", local=TRUE) ###imports metric comparison server code
 	source("../../server/stature.r", local=TRUE) ###imports stature outlier comparison server code
 	source("../../server/twod.r", local=TRUE) ###imports two-dimensional scomparison server code
-	source("../../server/antestat_single.r", local=TRUE) ###imports two-dimensional scomparison server code
-	source("../../server/antestat_multiple.r", local=TRUE) ###imports two-dimensional scomparison server code
+	source("../../server/antestat_single.r", local=TRUE) ###imports single comparison antemortem stature code
+	source("../../server/antestat_multiple.r", local=TRUE) ###imports multiple comparison antemortem stature code
 	################stops the shiny app when closing session
 	session$onSessionEnded(function() { stopApp()})
 
@@ -39,8 +35,6 @@ shinyServer(function(input, output, session) {
 		unlink(sessiontemp, recursive = TRUE)    
 	})
 	
-
-
 	#download handlers for files on the help page
 	output$postmortem_template <- downloadHandler(
 		filename <- function() {
@@ -70,7 +64,6 @@ shinyServer(function(input, output, session) {
 		},
 	)  			
 
-
 	output$example_data <- downloadHandler(
 		filename <- function() {
 			"Example_Data.zip"
@@ -80,13 +73,8 @@ shinyServer(function(input, output, session) {
 		},
 	)  	
 
-
 	output$measurement_conversion_table <- DT::renderDataTable(read.table(system.file("extdata", "Standardized_Measurements.csv", package = "OsteoShiny"), header = TRUE, sep=","), options = list(lengthMenu = c(10,20,30,40,50), pageLength = 20), rownames = FALSE)
 	
-
-	  
-
-
 	observeEvent(input$Create_Desktop_Icon, {
 		if(Sys.info()[['sysname']] == "Windows") {
 
@@ -96,28 +84,25 @@ shinyServer(function(input, output, session) {
 			pathname <- paste('"', paste(gsub("/Documents", "", file.path(path.expand("~"), "Desktop") ), "OsteoSort.lnk", sep = "/"), '"', sep="")
 			vbs <- paste('"', system.file("vbs/createLink.vbs", package = "OsteoShiny"), '"', sep="")
 			system(paste("cscript", vbs, pathname, target, arguments, icon, sep=" "))
-
 		}
 		if(Sys.info()[['sysname']] == "Linux") {
 			icon_name <- "OsteoSort.desktop"
-			cat(		paste(
-						"[Desktop Entry]\nEncoding=UTF-8\nTerminal=true\nType=Application\nCategories=Application\nName=OsteoSort\n",
-						"Version=",	packageVersion("OsteoShiny"),"\n",
+			cat(		
+				paste(
+					"[Desktop Entry]\nEncoding=UTF-8\nTerminal=true\nType=Application\nCategories=Application\nName=OsteoSort\n",
+					"Version=",	packageVersion("OsteoShiny"),"\n",
 
-						 "Icon=",		system.file("OsteoSort/www/OsteoSort.png", package = "OsteoShiny"),"\n",
-						 "Exec=",		paste(file.path(R.home("bin"), "R"), "-e", "library(OsteoShiny);OsteoSort()", sep=" ")
-					,sep=""),#paste
+					"Icon=",		system.file("OsteoSort/www/OsteoSort.png", package = "OsteoShiny"),"\n",
+					"Exec=",		paste(file.path(R.home("bin"), "R"), "-e", "library(OsteoShiny);OsteoSort()", sep=" ")
+				,sep=""),#paste
 				file = paste(file.path(path.expand("~"), "Desktop"), "OsteoSort.desktop", sep = "/")
 			)#cat
 			Sys.chmod(paste(file.path(path.expand("~"), "Desktop"), "OsteoSort.desktop", sep="/"), mode = "0777", use_umask = TRUE)
 		}
-		if(Sys.info()[['sysname']] != "Linux" && Sys.info()[['sysname']] != "Windows") { #temporary for mac
+		if(Sys.info()[['sysname']] != "Linux" && Sys.info()[['sysname']] != "Windows") { #.command for mac
 			icon_name <- "OsteoSort.sh"
-			cat(paste(file.path(R.home("bin"), "R"), "-e", "'library(OsteoShiny);OsteoSort()'", sep=" "), file = paste(file.path(path.expand("~"), "Desktop"), "OsteoSort.sh", sep = "/"))
+			cat(paste(file.path(R.home("bin"), "R"), "-e", "'library(OsteoShiny);OsteoSort()'", sep=" "), file = paste(file.path(path.expand("~"), "Desktop"), "OsteoSort.command", sep = "/"))
 			Sys.chmod(paste(file.path(path.expand("~"), "Desktop"), "OsteoSort.sh", sep="/"), mode = "0777", use_umask = TRUE)
 		}
-
 	})
-	
-
 })
